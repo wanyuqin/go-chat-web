ebe9e48ca77da89929ba7327bfca814507ce6937
Ethan <EthanLeo@yeah.net>
2023-04-17 12:54:58 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+<Terminal></Terminal>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Terminal from './components/Terminal.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+     Terminal
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
+/* } */
+
+.container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
 </style>
